a6a9211f5e0c11948461d6b1fca8ecae35e53053
Balin Warren <balin.warren@gmail.com>
2023-06-08 08:21:06 -0700

    Added font awesome icon kit to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+
+fontAwesome = document.createElement('script');
+fontAwesome.src = "https://kit.fontawesome.com/ab87087bfd.js";
+fontAwesome.crossorigin = "anonymous";
+document.head.appendChild(fontAwesome);
